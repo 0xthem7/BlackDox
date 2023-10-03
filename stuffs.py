f45175de648aeca5f843a53328599779ef6e860f
0xthem7 <openmyaddress@gmail.com>
2023-10-03 09:19:41 +0545

    Updates

--- stuffs.py
+++ stuffs.py
@@ -43,14 +43,14 @@ class blackDox:
 {self.RED}.:.{self.RESET}  {self.CYAN}Select any option:{self.RESET}
          
 {self.YELLOW}[01]{self.RESET}{self.MAGENTA}internal{self.RESET} \t{self.YELLOW}[02]{self.MAGENTA}external{self.RESET}
-{self.YELLOW}[03]{self.RESET}{self.MAGENTA}help{self.RESET}\t{self.YELLOW}[04]{self.MAGENTA}exit{self.RESET}
+{self.YELLOW}[03]{self.RESET}{self.MAGENTA}help{self.RESET}\t{self.YELLOW}[00]{self.MAGENTA}exit{self.RESET}
 
  ''')
         self.choice = input(f"{self.RED}[{self.RESET}~{self.RED}]{self.RESET} {self.GREEN}Select an option:{self.RESET} ")
         if self.choice == "1" or "01":
             self.target = input(f"{self.RED}[{self.RESET}~{self.RED}]{self.RESET} {self.GREEN}Target url:{self.RESET} ")
-            os.system(f'nmap -sC -sV -T4 {self.target} -oN {self.target}.result')
-
+            os.system(f'nmap -A -vv -T4 {target} -oN {target}.result')
+            os.system(f'scripts/report_nmap {target}.result')
         elif self.choice == "2" or "02":
             self.target = input(f"{self.RED}[{self.RESET}~{self.RED}]{self.RESET} {self.GREEN}Target :{self.RESET} ")
             os.system(f'wpscan --url {self.target}  --random-user-agent -o {self.target}.result')
@@ -62,18 +62,13 @@ class blackDox:
     Current running version - 0.1
     help - To show the help command
     internal [target] - Scan internal networks / ports
-    externam [target] - Scan website
+    external [target] - Scan website
     makeItEasy Converts terminal line into selection; Easy to operate
     exit - To exit the program              
 ''')
         elif self.choice == "00" or "0":
             print(f'[{self.YELLOW}!{self.RESET}] Have a good time {self.GREEN}Hax0r{self.RESET}')
             exit(0)
-
-                
-
-
-
 
 
     def commandHandler(self,command):
@@ -96,7 +91,9 @@ class blackDox:
         elif 'internal' in command:
             try : 
                 command = command.split(' ')[1]
-                os.system(f'nmap -sC -sV -T4 {command} -oN {command}.result')
+                #os.system(f'nmap -A -vv -T4 {command} -oN {command}.result')
+                os.system(f'scripts/report_nmap {command}.result')
+
             except:
                 print(f'{self.CYAN}[{self.RED}~{self.GREEN}]{self.RED}Target not found !!!')
                 print(f'{self.CYAN}[{self.YELLOW}?{self.GREEN}]{self.YELLOW}Target : {self.RESET}',end='')
@@ -104,12 +101,11 @@ class blackDox:
         elif 'external'  in command:
             try : 
                 command = command.split(' ')[1]
-                os.system(f'wpscan --url {command}  --random-user-agent -o {command}.result')
+                os.system(f'wpscan --url {command}  --random-user-agent -o {command}.wordpress.result')
             except:
                 print(f'{self.CYAN}[{self.RED}~{self.GREEN}]{self.RED}Target not found !!!')
                 print(f'{self.CYAN}[{self.YELLOW}?{self.GREEN}]{self.YELLOW}Target : {self.RESET}',end='')
                 command = input()
         elif 'makeItEasy' or 'makeiteasy'in command:
-            print('I am here')
             self.makeItEasy()
         
