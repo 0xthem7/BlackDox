87cb10d66ace4b6ed1e9e330ab2b31fa25c8052c
0xthem7 <openmyaddress@gmail.com>
2023-10-08 15:20:08 +0545

    Some updates

--- stuffs.py
+++ stuffs.py
@@ -39,23 +39,25 @@ class blackDox:
         print(f'{self.GREEN}{self.blackDox_art}',end = "")
         print(f'\t\t\t\t\t\t\t{self.RED}version 0.1 {self.RESET}')
         print(f"{self.YELLOW}[+]{self.RESET} {self.GREEN}Tool created by Apocalypse{self.RESET}")
-        print(f'''
-{self.RED}.:.{self.RESET}  {self.CYAN}Select any option:{self.RESET}
-         
-{self.YELLOW}[01]{self.RESET}{self.MAGENTA}internal{self.RESET} \t{self.YELLOW}[02]{self.MAGENTA}external{self.RESET}
-{self.YELLOW}[03]{self.RESET}{self.MAGENTA}help{self.RESET}\t{self.YELLOW}[00]{self.MAGENTA}exit{self.RESET}
+        print(f"{self.RED}.:.{self.RESET}  {self.CYAN}Select any option:{self.RESET}")
+        print(f"{self.YELLOW}[01]{self.RESET}{self.MAGENTA}internal{self.RESET} \t{self.YELLOW}[02]{self.MAGENTA}external{self.RESET}")
+        print(f"{self.YELLOW}[03]{self.RESET}{self.MAGENTA}injection{self.RESET} \t{self.YELLOW}[03]{self.RESET}{self.MAGENTA}help{self.RESET}")
+        print(f"{self.YELLOW}[00]{self.MAGENTA}exit{self.RESET}")
 
- ''')
+
         self.choice = input(f"{self.RED}[{self.RESET}~{self.RED}]{self.RESET} {self.GREEN}Select an option:{self.RESET} ")
         if self.choice == "1" or "01":
             self.target = input(f"{self.RED}[{self.RESET}~{self.RED}]{self.RESET} {self.GREEN}Target url:{self.RESET} ")
-            os.system(f'nmap -A -vv -T4 {target} -oN {target}.result')
-            os.system(f'scripts/report_nmap {target}.result')
-        elif self.choice == "2" or "02":
+            os.system(f'nmap -A -vv -T4 {self.target} -oN reports/{self.target}.result')
+            os.system(f'scripts/report_nmap {self.target}.result')
+
+
+
+        elif self.choice == "2" or self.choice == "02":
             self.target = input(f"{self.RED}[{self.RESET}~{self.RED}]{self.RESET} {self.GREEN}Target :{self.RESET} ")
-            os.system(f'wpscan --url {self.target}  --random-user-agent -o {self.target}.result')
+            os.system(f'wpscan --url {self.target} --random-user-agent -o reports/{self.target}.result')
             
-        elif self.choice == "3" or "03":
+        elif self.choice == "3" or self.choice == "03":
             print('''
       .:. Black Dox help menu
     
@@ -63,12 +65,16 @@ class blackDox:
     help - To show the help command
     internal [target] - Scan internal networks / ports
     external [target] - Scan website
+    injection [target] - check for sql injection attacks
     makeItEasy Converts terminal line into selection; Easy to operate
     exit - To exit the program              
 ''')
-        elif self.choice == "00" or "0":
+        elif self.choice == "00" or  self.choice == "0":
             print(f'[{self.YELLOW}!{self.RESET}] Have a good time {self.GREEN}Hax0r{self.RESET}')
             exit(0)
+        
+        else:
+            print('command not recognized')
 
 
     def commandHandler(self,command):
@@ -83,15 +89,19 @@ class blackDox:
     Current running version - 0.1
     help - To show the help command
     internal [target] - Scan internal networks / ports
-    externam [target] - Scan website
+    external [target] - Scan website
+    injection [target] - check for sql injection attacks
     makeItEasy Converts terminal line into selection; Easy to operate
     exit - To exit the program              
 ''')
+            
+
+
 
         elif 'internal' in command:
             try : 
                 command = command.split(' ')[1]
-                #os.system(f'nmap -A -vv -T4 {command} -oN {command}.result')
+                os.system(f'nmap -A -vv -T4 {command} -oN reports/{command}.result')
                 os.system(f'scripts/report_nmap {command}.result')
 
             except:
@@ -101,11 +111,13 @@ class blackDox:
         elif 'external'  in command:
             try : 
                 command = command.split(' ')[1]
-                os.system(f'wpscan --url {command}  --random-user-agent -o {command}.wordpress.result')
+                os.system(f'wpscan --url {command} --random-user-agent -o reports/{command}.wordpress.result')
             except:
                 print(f'{self.CYAN}[{self.RED}~{self.GREEN}]{self.RED}Target not found !!!')
                 print(f'{self.CYAN}[{self.YELLOW}?{self.GREEN}]{self.YELLOW}Target : {self.RESET}',end='')
                 command = input()
-        elif 'makeItEasy' or 'makeiteasy'in command:
+        elif 'makeItEasy'in command or  'makeiteasy'in command:
             self.makeItEasy()
-        
+        else:
+            pass
+
